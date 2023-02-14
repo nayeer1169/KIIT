7abd39d314f34b0d4b9e906c1459682f5557f3a5
Nayeer Naushad <121220518+nayeer1169@users.noreply.github.com>
2023-02-14 22:58:37 +0530

    Add files via upload

---
+++ 4th SEMESTER/JAVA/CODE/ArithmeticOpr.java
@@ -0,0 +1,14 @@
+//import java.util.Scanner;
+public class ArithmeticOpr { //used for adding subtracting etc etc two number
+    public static void main(String[] args){
+        System.out.println( 9 + 5 );
+        System.out.println( 9 - 5 );
+        System.out.println( 9 * 5 );
+        System.out.println( 9 / 5 );
+        System.out.println( 9 % 5 );
+
+        //char + int
+        System.out.println( 'A' + 1 ); //Ascii value will  automatically come
+        System.out.println( 'a' + 'A' );  //Ascii value of a=97  A=65
+    }
+}
---
+++ 4th SEMESTER/JAVA/CODE/DoubleInt.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class DoubleInt {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        System.out.println(9/5);
+        System.out.println(9.0/5);
+    }
+}
